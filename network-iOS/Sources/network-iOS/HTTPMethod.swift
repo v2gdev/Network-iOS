2bd8e40a0405737f9693872975de0d38887a1eb6
jiheeHwang <jhhwang@autocrypt.io>
2024-05-07 17:41:29 +0900

    [✨] - HTTPHeader, Headers, HTTPMethod 구현

---
+++ network-iOS/Sources/network-iOS/HTTPMethod.swift
@@ -0,0 +1,27 @@
+//
+//  HTTPMethod.swift
+//
+//
+//  Created by Jihee hwang on 4/22/24.
+//
+
+import Foundation
+
+public struct HTTPMethod:
+    RawRepresentable,
+    Equatable,
+    Hashable
+{
+
+    public static let delete = HTTPMethod(rawValue: "DELETE")
+    public static let get = HTTPMethod(rawValue: "GET")
+    public static let post = HTTPMethod(rawValue: "POST")
+    public static let put = HTTPMethod(rawValue: "PUT")
+
+    public let rawValue: String
+    
+    public init(rawValue: String) {
+        self.rawValue = rawValue
+    }
+    
+}
---
+++ network-iOS/Sources/network-iOS/Header/HTTPHeader.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jihee hwang on 4/22/24.
+//
+
+import Foundation
+
+public struct HTTPHeader:
+    Hashable,
+    PropertyBuilderCompatible
+{
+    public var name: String = ""
+    public var value: String = ""
+    
+    public init(name: String, value: String) {
+        self.name = name
+        self.value = value
+    }
+    
+    public init () {}
+}
