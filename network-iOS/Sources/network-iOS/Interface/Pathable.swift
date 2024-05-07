7abbda6a0ecdda5beeb71362baf8b5a0af6b2ba1
jiheeHwang <jhhwang@autocrypt.io>
2024-05-07 17:42:42 +0900

    [✨] - NetworkError 및 Endpoint 생성 객체 구현

---
+++ network-iOS/Sources/network-iOS/Interface/Pathable.swift
@@ -0,0 +1,12 @@
+//
+//  Pathable.swift
+//  
+//
+//  Created by Jihee hwang on 4/23/24.
+//
+
+import Foundation
+
+public protocol Pathable {
+    var path: String { get }
+}
---
+++ network-iOS/Sources/network-iOS/Interface/URLConvertible.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jihee hwang on 4/22/24.
+//
+
+import Foundation
+
+public protocol URLConvertible {
+    func asURL() throws -> URL
+}
+
