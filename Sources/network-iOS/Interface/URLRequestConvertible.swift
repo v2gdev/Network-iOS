a4ae345e113ca1317ec483b37eaaef0c2ee0e39d
jiheeHwang <jhhwang@autocrypt.io>
2024-06-07 16:17:44 +0900

    [✨] - QueryPram 변환 및 Request Body 없는 경우에 대한 핸들링 함수 추가

--- Sources/network-iOS/Interface/URLRequestConvertible.swift
+++ Sources/network-iOS/Interface/URLRequestConvertible.swift
@@ -8,13 +8,21 @@
 import Foundation
 
 public protocol URLRequestConvertible {
+    /// Body가 있는 경우
     static func asURLRequest<E: Encodable>(api: URL,
-                                    method: HTTPMethod,
-                                    headers: HTTPHeaders?,
-                                    body: E?,
-                                    timeout: TimeInterval
+                                           method: HTTPMethod,
+                                           headers: HTTPHeaders?,
+                                           body: E?,
+                                           timeout: TimeInterval
     ) async throws -> URLRequest
     
+    /// Body가 없는 경우
+    static func asURLRequest(api: URL,
+                             method: HTTPMethod,
+                             headers: HTTPHeaders?,
+                             timeout: TimeInterval
+    ) -> URLRequest
+    
     /// String to ImageURL
-    static func asImageURLRequest(imageURL: URL) async throws -> URLRequest
+    static func asImageURLRequest(imageURL: URL) -> URLRequest
 }
